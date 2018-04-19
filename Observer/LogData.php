69feb2f3911ce6b025b8afa475f253f37065c6cf
Cédric Stanislas <nori971@gmail.com>
2018-04-19 23:43:15 +0200

    anko

---
+++ Observer/LogData.php
@@ -0,0 +1,10 @@
+<?php
+namespace MxCs\LogAccess\Observer;
+class LogData implements \Magento\Framework\Event\ObserverInterface
+{
+	public function execute(\Magento\Framework\Event\Observer $observer)
+	{
+/*		Met biten aw la
+*/		return $this;
+	}
+}
